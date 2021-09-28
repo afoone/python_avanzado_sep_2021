92bcd08002d6f9e66be37ece6e52edb45016aaf9
Alfonso Tienda <afoone@hotmail.com>
2021-09-28 11:03:41 +0200

    primera clase

---
+++ pycharm/pycharm_intro/suma.py
@@ -0,0 +1,16 @@
+def suma(a, b):
+    """
+    Suma dos números
+    >>> suma(3,3)
+    6
+    :param a:
+    :param b:
+    :return:
+    """
+    print("imprimiendo a y b", a, b)
+    breakpoint()
+    return a+b
+
+
+print(suma(4, 5))
+
